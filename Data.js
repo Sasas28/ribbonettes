45f2a0d52fea8c5f53e35de938a156e3844dbc91
Sasas28 <ajasterzamora28@gmail.com>
2023-09-25 05:50:59 +0800

    added new file

---
+++ Data.js
@@ -0,0 +1,30 @@
+let shopItemsData = [
+    {
+        id: 1,
+        name: "Choco Fudge",
+        price: "89.00",
+        img: "img/choco-fudge.jpg"
+    },
+
+    {
+        id: 2,
+        name: "Crinkles",
+        price: "39.00",
+        img: "img/crinkles.jpg"
+    },
+
+    {
+        id: 3,
+        name: "Hopia baboy",
+        price: "65.00",
+        img: "img/hopia-baboy.jpg"
+    },
+
+    {
+        id: 4,
+        name: "Hopia de leche",
+        price: "89.00",
+        img: "img/hopia-de-leche.jpg"
+    },
+
+];
